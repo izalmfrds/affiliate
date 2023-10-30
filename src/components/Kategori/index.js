97dccb405597ba87e69475e0f007a530a6067f29
izalmfrds <67822136+izalmfrds@users.noreply.github.com>
2023-10-30 20:52:38 +0700

    Update index.js

--- src/components/Kategori/index.js
+++ src/components/Kategori/index.js
@@ -4,42 +4,42 @@ import kategoriimg from "@/Assets/Images/kategoribaju.png";
 
 export default function Kategori() {
   return (
-    <div className="grid grid-cols-4 gap-4  bg-white shadow-md mx-2">
-      <div className=" justify-items-center items-center text-center m-auto my-4">
+    <div className="grid grid-cols-4 gap-4  bg-white shadow-md mx-2 my-4">
+      <div className=" justify-items-center items-center text-center m-auto my-2">
         <Link href="/Kategori_pages">
           <Image className="w-12 h-12  " src={kategoriimg} alt="iconkategori" />
           <h1>Baju</h1>
         </Link>
       </div>
 
-      <div className=" justify-items-center items-center text-center m-auto my-4">
+      <div className=" justify-items-center items-center text-center m-auto my-2">
         <Link href="/Admin">
           <Image className="w-12 h-12 " src={kategoriimg} alt="iconkategori" />
           <h1>Baju</h1>
         </Link>
       </div>
 
-      <div className=" justify-items-center items-center text-center m-auto my-4">
+      <div className=" justify-items-center items-center text-center m-auto my-2">
         <Image className="w-12 h-12 " src={kategoriimg} alt="iconkategori" />
         <h1>Baju</h1>
       </div>
-      <div className=" justify-items-center items-center text-center m-auto my-4">
+      <div className=" justify-items-center items-center text-center m-auto my-2">
         <Image className="w-12 h-12 " src={kategoriimg} alt="iconkategori" />
         <h1>Baju</h1>
       </div>
-      <div className=" justify-items-center items-center text-center m-auto my-4">
+      <div className=" justify-items-center items-center text-center m-auto my-2">
         <Image className="w-12 h-12 " src={kategoriimg} alt="iconkategori" />
         <h1>Baju</h1>
       </div>
-      <div className=" justify-items-center items-center text-center m-auto my-4">
+      <div className=" justify-items-center items-center text-center m-auto my-2">
         <Image className="w-12 h-12 " src={kategoriimg} alt="iconkategori" />
         <h1>Baju</h1>
       </div>
-      <div className=" justify-items-center items-center text-center m-auto my-4">
+      <div className=" justify-items-center items-center text-center m-auto my-2">
         <Image className="w-12 h-12 " src={kategoriimg} alt="iconkategori" />
         <h1>Baju</h1>
       </div>
-      <div className=" justify-items-center items-center text-center m-auto my-4">
+      <div className=" justify-items-center items-center text-center m-auto my-2">
         <Image className="w-12 h-12 " src={kategoriimg} alt="iconkategori" />
         <h1>Baju</h1>
       </div>
