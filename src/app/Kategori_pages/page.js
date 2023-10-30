70a4b9f98ab73a637fb8231da3cbf00178b38932
izalmfrds <67822136+izalmfrds@users.noreply.github.com>
2023-10-30 20:44:41 +0700

    update header kategori

--- src/app/Kategori_pages/page.js
+++ src/app/Kategori_pages/page.js
@@ -1,9 +1,15 @@
+import Image from "next/image";
+import Back from "../../Assets/Icon/back.png";
 import Search from "@/components/Search";
 import Product from "@/components/Product";
 
 export default function Kategori() {
   return (
     <>
+      <div className="flex flex-row items-center mt-2 gap-24">
+        <Image src={Back} className="w-8 h-8 ml-2" alt="iconback" />
+        <h1 className="font-bold text-2xl">Kategori Baju</h1>
+      </div>
       <div className="sticky top-0 bg-white pt-2 pb-2 shadow-lg">
         <Search />
       </div>
