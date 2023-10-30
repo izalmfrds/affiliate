ce6bb150dc3d801d9873749ade9df7a39c94f9fd
izalmfrds <67822136+izalmfrds@users.noreply.github.com>
2023-10-30 20:47:39 +0700

    Update page.js

--- src/app/Kategori_pages/page.js
+++ src/app/Kategori_pages/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import Back from "../../Assets/Icon/back.png";
 import Search from "@/components/Search";
 import Product from "@/components/Product";
@@ -6,11 +7,13 @@ import Product from "@/components/Product";
 export default function Kategori() {
   return (
     <>
-      <div className="flex flex-row items-center mt-2 gap-24">
-        <Image src={Back} className="w-8 h-8 ml-2" alt="iconback" />
-        <h1 className="font-bold text-2xl">Kategori Baju</h1>
-      </div>
       <div className="sticky top-0 bg-white pt-2 pb-2 shadow-lg">
+        <div className="flex flex-row items-center gap-24 pb-2">
+          <Link href="/">
+            <Image src={Back} className="w-8 h-8 ml-2" alt="iconback" />
+          </Link>
+          <h1 className="font-bold text-2xl">Kategori Baju</h1>
+        </div>
         <Search />
       </div>
       <Product />
