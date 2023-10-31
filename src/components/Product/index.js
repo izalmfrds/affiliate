366d5abc3b677d31bee9b257ab0e785153722c43
izalmfrds <67822136+izalmfrds@users.noreply.github.com>
2023-10-31 14:59:44 +0700

    gap

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -7,40 +7,51 @@ import React from "react";
 const index = ({ api }) => {
   console.log(api);
   return (
-    <div>
-      <div className="border-2 border-black mb-4 mx-2 bg-white">
+    <div className="">
+      <div className="mb-4 mx-2">
         {api.map((produk, index) => {
           return (
-            <div key={index}>
-              <img className="w-full h-auto" src={produk.image} alt="test" />
-              <div className="m-2 ">
-                <p>{produk.id}</p>
-                <p>{produk.title}</p>
+            <div key={index} className="border-2 border-black my-4 ">
+              <div className="p-12">
+                <Image
+                  className="w-full h-auto"
+                  src={produk.image}
+                  alt="test"
+                  width={250}
+                  height={250}
+                />
               </div>
-              <div className="flex flex-rows gap-2 items-center m-2">
-                <button className="flex flex-row w-full border-2 border-black">
-                  <Image
-                    className="w-12 h-12 mx-2"
-                    src={shopee}
-                    alt="icon shopee"
-                  />
-                  <h1 className="h-6 w-full text-lg text-center m-auto">
-                    {" "}
-                    Shopee
-                  </h1>
-                </button>
 
-                <button className="flex flex-row w-full border-2 border-black">
-                  <Image
-                    className="w-12 h-12 mx-2"
-                    src={tokopedia}
-                    alt="icon tokped"
-                  />
-                  <h1 className="h-6 w-full text-lg text-center m-auto">
-                    {" "}
-                    Tokopedia
-                  </h1>
-                </button>
+              <div className="bg-purple-300 py-2">
+                <div className="m-2 ">
+                  <p>{produk.id}</p>
+                  <p>{produk.title}</p>
+                </div>
+                <div className="flex flex-rows gap-2 items-center m-2">
+                  <button className="flex flex-row w-full border-2 border-black">
+                    <Image
+                      className="w-12 h-12 mx-2"
+                      src={shopee}
+                      alt="icon shopee"
+                    />
+                    <h1 className="h-6 w-full text-lg text-center m-auto">
+                      {" "}
+                      Shopee
+                    </h1>
+                  </button>
+
+                  <button className="flex flex-row w-full border-2 border-black">
+                    <Image
+                      className="w-12 h-12 mx-2"
+                      src={tokopedia}
+                      alt="icon tokped"
+                    />
+                    <h1 className="h-6 w-full text-lg text-center m-auto">
+                      {" "}
+                      Tokopedia
+                    </h1>
+                  </button>
+                </div>
               </div>
             </div>
           );
